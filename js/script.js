a08ca6f20279a8b9ec8dbe470f30417d622b4c28
Abdullah Al Raju <abdullahalraju36@gmail.com>
2023-08-05 13:29:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+new WOW().init();
+
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+  
+
+		
+		
